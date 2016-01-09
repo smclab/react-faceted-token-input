4e7379638119a37ce47174e846c0c7b80e4d9e27
Pier Paolo Ramon <ramonpierre@gmail.com>
2016-01-09 12:53:37 +0100

    Linting pass

--- src/key-utils.js
+++ src/key-utils.js
@@ -7,26 +7,20 @@ const IS_MAC = (UA.indexOf('macintosh') >= 0);
 
 export const isCtrlKey = event => event[ IS_MAC ? 'metaKey' : 'ctrlKey' ];
 
-export const isOnlyCtrlKey = IS_MAC ?
-  (event => event.metaKey && !event.ctrlKey) :
-  (event => event.ctrlKey && !event.metaKey);
+export const isOnlyCtrlKey = IS_MAC
+  ? event => event.metaKey && !event.ctrlKey
+  : event => event.ctrlKey && !event.metaKey;
 
-export const isSelectToHome = IS_MAC ?
-  (event => (
-    event.metaKey && event.shiftKey && !event.ctrlKey && !event.altKey &&
-    (event.which === LEFT)
-  )) :
-  (
-    // TODO: Windows support
-    event => false
-  );
+export const isSelectToHome = IS_MAC
+  ? ({ metaKey, shiftKey, ctrlKey, altKey, which }) =>
+    metaKey && shiftKey && !ctrlKey && !altKey && (which === LEFT)
+  // TODO: Windows support
+  : event => false
+;
 
-export const isSelectToEnd = IS_MAC ?
-  (event => (
-    event.metaKey && event.shiftKey && !event.ctrlKey && !event.altKey &&
-    (event.which === RIGHT)
-  )) :
-  (
+export const isSelectToEnd = IS_MAC
+  ? ({ metaKey, shiftKey, ctrlKey, altKey, which }) =>
+    metaKey && shiftKey && !ctrlKey && !altKey && (which === RIGHT)
     // TODO: Windows support
-    event => false
-  );
+  : event => false
+;
