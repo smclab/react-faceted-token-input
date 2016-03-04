e3ea9591112f4a04e84eaba65671e8a1a91ed79d
Gianpietro Benincà <gianpietro.beninca@gmail.com>
2016-03-04 12:23:10 +0100

    Updated RTL_RANGES to the unicode 8.0.0 standard

--- src/key-utils.js
+++ src/key-utils.js
@@ -7,10 +7,16 @@ export const IS_MAC = (UA.indexOf('macintosh') >= 0);
 
 export const isCtrlKey = event => event[ IS_MAC ? 'metaKey' : 'ctrlKey' ];
 
+// Ranges taken from the Unicode DerivedBidiClass.txt version 8.0.0
 const CHARACTERS_LTR_RANGES = 'A-Za-z\u00C0-\u00D6\u00D8-\u00F6\u00F8-\u02B8' +
   '\u0300-\u0590\u0800-\u1FFF\u2C00-\uFB1C\uFDFE-\uFE6F\uFEFD-\uFFFF';
 
-const CHARACTERS_RTL_RANGES = '\u0591-\u07FF\uFB1D-\uFDFD\uFE70-\uFEFC';
+const CHARACTERS_RTL_RANGES = '\u0591-\u05C7\u0600-\u06FF\u0700-\u074F' +
+  '\u0750-\u077F\u0780-\u07BF\u08A0-\u08FF\uFB50-\uFDCF\uFDF0-\uFDFF' +
+  '\uFE70-\uFEFF\u1EE00-\u1EEFF\u0590-\u05FF\u07C0-\u07FF\u10800-\u1083F' +
+  '\u10900-\u1091F\u10920-\u1093F\u10980-\u1099F\u109A0-\u109FF' +
+  '\u10A00-\u10A5F\u0800-\u089F\uFB1D-\uFB4F\u10840-\u10FFF\u1E800-\u1EDFF' +
+  '\u1EF00-\u1EFFF\u0001EE00-\u0001EEFF';
 
 const DIRECTION_CHECK_RE = new RegExp('^[^'+CHARACTERS_LTR_RANGES+']*['+CHARACTERS_RTL_RANGES+']');
 
